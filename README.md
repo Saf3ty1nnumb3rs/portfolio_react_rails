a6ed956d9b77ac6fcc5bd7d661db179c90c9465c
Josh Sample <joshuawsample@gmail.com>
2018-04-19 16:31:50 -0400

    Updated about section, updated readme

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
-# README
+# This portfolio is a work in progress!!!
 ![Image of iCon](./client/src/images/icon.jpg)
 ![Image of WISHkey](./client/src/images/wishkey.jpg)
 ![Image of vagabond](./client/src/images/vagabond.jpg)
 ![Image of daytrippr](./client/src/images/daytrippr.jpg)
-This README would normally document whatever steps are necessary to get the
-application up and running.
 
-Things you may want to cover:
+* <a name="JoshLink" href="https://www.linkedin.com/in/joshsample/">
+Linked In</a>
+## This portfolio was built using:
 
-* Ruby version
+* PostgreSQL
 
-* System dependencies
+* Ruby
 
-* Configuration
+* Ruby on Rails
 
-* Database creation
+* ReactJS
 
-* Database initialization
+* NodeJS
 
-* How to run the test suite
+* JavaScript
 
-* Services (job queues, cache servers, search engines, etc.)
+* HTML5
 
-* Deployment instructions
+* CSS3
 
-* ...
+* Styled Components
--- client/src/components/viewcomponents/About.js
+++ client/src/components/viewcomponents/About.js
@@ -1,13 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import profile from "../../images/profile.jpg";
+import styled from "styled-components";
+
+const AboutWrap = styled.div`
+  img {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
+  p {
+    position: relative;
+    color: white;
+    z-index: 100;
+    margin:2vw auto;
+    top: 20vh;
+    left: 0;
+    right: 0;
+    font-size: 2.3vw;
+    width: 80vw;
+    text-shadow: 1px black;
+  }
+`;
 
 class About extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <AboutWrap>
+        <p>
+          Hi! My name is Josh. Thanks for taking the time to check out what I
+          do. I'm utilizing a love of technology and design to bring about change to
+          our digital world. I absolutely love design, but I am also immensely
+          fascinated by backend development and the possibilities it creates.
+          The greatest reward comes from combining beautiful and elegant design
+          and a powerful and seamless backend to create something highly
+          functional and enjoyable to use. The possibilities are endless; the
+          learning and creating will never stop. Let's go.
+        </p>
+        <p>
+          When I'm not showing people pictures of my daughter Auguste, I'm
+          hanging out with her and her beautiful mother.
+        </p>
+        <p> I like squirrels.</p>
+        <p> A lot.</p>
+        <img src={profile} alt="Me" />
+      </AboutWrap>
+    );
+  }
 }
 
 export default About;
