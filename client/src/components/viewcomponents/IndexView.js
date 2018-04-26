c66463799edafc725a220eab44ae4ad85319b4e7
Josh Sample <joshuawsample@gmail.com>
2018-04-26 00:39:28 -0400

    Light bulb

--- client/src/components/viewcomponents/IndexView.js
+++ client/src/components/viewcomponents/IndexView.js
@@ -3,58 +3,60 @@ import ProjectList from "../projectcomponents/ProjectList";
 import styled from "styled-components";
 import axios from "axios";
 import katsuya from "../../images/katsuya1.jpg";
+import bulb from "../../images/lightbulb.mp4";
 import TopSection from "../TopSection";
 import OverlayNav from "./OverlayNav";
 
-
 const IndexWrap = styled.div`
-  img.back {
+  video.back {
     position: relative;
+    left: -5vw;
     height: auto;
-    width: 100vw;
+    width: 105vw;
     z-index: -1000;
     box-shadow: 2px 4px 12px black;
   }
 `;
 
 class IndexView extends Component {
-  
-    state = {
-        projects: [],
-        error: ""
-      };
-    
-      componentDidMount() {
-        this.getAllProjects();
-      }
-    
-      getAllProjects = async () => {
-        try {
-          const response = await axios.get("api/projects");
-          console.log(response.data);
-          this.setState({
-            projects: response.data.projects
-          });
-        } catch (err) {
-          console.log(err);
-          this.setState({ error: err.message });
-        }
-      };
-    
+  state = {
+    projects: [],
+    error: ""
+  };
+
+  componentDidMount() {
+    this.getAllProjects();
+  }
+
+  getAllProjects = async () => {
+    try {
+      const response = await axios.get("api/projects");
+      console.log(response.data);
+      this.setState({
+        projects: response.data.projects
+      });
+    } catch (err) {
+      console.log(err);
+      this.setState({ error: err.message });
+    }
+  };
 
   render() {
     return (
       <div>
         {this.props.showOverlay ? (
-          <OverlayNav {...this.props} toggleOverlay={this.props.toggleOverlay}/>
+          <OverlayNav
+            {...this.props}
+            toggleOverlay={this.props.toggleOverlay}
+          />
         ) : (
           <IndexWrap>
-            <img className="back" src={katsuya} alt="katsuya" />
+            <video className="back" preload="auto" autoPlay loop>
+              <source src={bulb} type="video/mp4" />
+            </video>
             <TopSection />
             <ProjectList projects={this.state.projects} />
-           
           </IndexWrap>
-          
         )}
       </div>
     );
